7fa3ac08d439b33a9e9c0b023612fd73f5034f69
hanszhou80 <hanszhou80@gmail.com>
2020-04-06 13:34:27 +0800

    1

--- src/main/java/com/xinbo/cloud/service/dubbo/merchant/service/impl/MerchantServiceApiImpl.java
+++ src/main/java/com/xinbo/cloud/service/dubbo/merchant/service/impl/MerchantServiceApiImpl.java
@@ -2,9 +2,15 @@ package com.xinbo.cloud.service.dubbo.merchant.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.xinbo.cloud.common.domain.common.Merchant;
+import com.xinbo.cloud.common.dto.PageDto;
+import com.xinbo.cloud.common.dto.common.MerchantDto;
 import com.xinbo.cloud.common.mapper.common.MerchantMapper;
 import com.xinbo.cloud.common.service.api.MerchantServiceApi;
+import com.xinbo.cloud.common.vo.PageVo;
+import com.xinbo.cloud.common.vo.common.MerchantVo;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
 
 /**
  * @author 熊二
@@ -15,12 +21,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Service(version = "A1.0.0")
 public class MerchantServiceApiImpl implements MerchantServiceApi {
 
-    @Autowired
-    private MerchantMapper merchantMapper;
 
     @Override
-    public Merchant getMerchant() {
-        Merchant merchant = merchantMapper.getByMerchantCode("xb120");
-        return merchant;
+    public MerchantDto get(long merchantId) {
+        return null;
+    }
+
+    @Override
+    public MerchantDto getByMerchantCode(String merchantCode) {
+        return null;
+    }
+
+    @Override
+    public PageDto<MerchantDto> getListPage(PageVo<MerchantVo> pageVo) {
+        return null;
+    }
+
+    @Override
+    public boolean add(Merchant merchant) {
+        return false;
+    }
+
+    @Override
+    public boolean update(Merchant merchant) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(int merchantId) {
+        return false;
+    }
+
+    @Override
+    public Map getMyCatDataNode() {
+        return null;
     }
 }
